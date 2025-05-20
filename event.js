6afc96f39767b7538010780221dd52f898956036
szaboistvanckik <szabo.istvan@ckik.hu>
2025-05-20 07:48:17 +0200

    További dizájn

--- event.js
+++ event.js
@@ -2,7 +2,7 @@ function gen_table() {
     const row = document.querySelector(".rows .row");
     const rows = document.querySelector(".rows");
 
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 10; i++) {
         const clone = row.cloneNode(true);
         rows.appendChild(clone);
     }
