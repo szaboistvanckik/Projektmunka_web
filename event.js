a0a485ba45fc8d2f067dfa75357603e8856c17ac
szaboistvanckik <szabo.istvan@ckik.hu>
2025-05-20 10:30:20 +0200

    Színek generálása javascript

--- event.js
+++ event.js
@@ -1,9 +1,28 @@
+const colors = [
+    'red', 'white', 'blue', 'pink',
+    'orange', 'purple', 'yellow', 'green'
+];
+
 function gen_table() {
+    
+
     const row = document.querySelector(".rows .row");
     const rows = document.querySelector(".rows");
+    
+    const f_colorpeg = row.querySelector('.colors .peg');
+    f_colorpeg.style.backgroundColor = colors[0];
+    row.style.display = 'flex';
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 1; i < 10; i++) {
         const clone = row.cloneNode(true);
+        const colorpeg = clone.querySelector('.colors .peg');
+        
+        if (i - 1 < colors.length - 1) {
+            colorpeg.style.backgroundColor = colors[i];
+        } else {
+            colorpeg.remove();
+        }
+        
         rows.appendChild(clone);
     }
 }
